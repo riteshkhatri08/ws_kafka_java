bba1f89397197dbe626a36e809929256d315d527
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-02-16 20:18:19 +0530

    Corrected imports

--- producer/app/src/main/java/producer/ProducerApp.java
+++ producer/app/src/main/java/producer/ProducerApp.java
@@ -5,14 +5,13 @@ package producer;
 
 import java.util.Properties;
 
-import javax.security.auth.callback.Callback;
-
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProducerApp {
 
