5d477d87d0643412f1a6a58c11c9f5881e01655a
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-02-16 20:25:31 +0530

    Added Kafka Consumer to subscribe to a topic

--- consumer/app/src/main/java/consumer/ConsumerApp.java
+++ consumer/app/src/main/java/consumer/ConsumerApp.java
@@ -3,7 +3,38 @@
  */
 package consumer;
 
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ConsumerApp {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerApp.class);
+
+    private final KafkaConsumer<String, String> consumer;
+
+    ConsumerApp() {
+        String bootstrapServers = "localhost:29093,localhost:29092,localhost:29091";
+        String groupId = "my-kafka-app";
+        String topic = "second_topic";
+
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        this.consumer = new KafkaConsumer<>(properties);
+
+        // subscribe consumer to our topic(s)
+        this.consumer.subscribe(Arrays.asList(topic));
+    }
 
     public String getGreeting() {
         return "Hello Consumer!";
